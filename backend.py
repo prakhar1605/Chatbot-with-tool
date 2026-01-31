be57304698588784f5a32cf6b1353e56a10ec36d
PRAKHAR PANDEY <prakhar9999pandey@gmail.com>
2026-01-31 19:00:34 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -4,7 +4,7 @@ from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_openai import ChatOpenAI
-#from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
 from langchain_community.tools import DuckDuckGoSearchRun
