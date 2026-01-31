f590924343ddb02f2fec8411f705142feb74fae3
PRAKHAR PANDEY <prakhar9999pandey@gmail.com>
2026-01-31 18:40:20 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -23,7 +23,10 @@ load_dotenv()
 llm = ChatOpenAI(
     model="openai/gpt-4o-mini",
     base_url="https://openrouter.ai/api/v1",
-    api_key=os.getenv("OPENROUTER_API_KEY"),
+    OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+
+if not OPENROUTER_API_KEY:
+    raise RuntimeError("OPENROUTER_API_KEY is not set"),
 )
 
 # -------------------
